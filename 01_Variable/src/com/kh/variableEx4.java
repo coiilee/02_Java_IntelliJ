1e751d636907a91d1e08f8e40df2873361a96114
coiilee <dakyung96@gmail.com>
2024-11-18 15:55:12 +0900

    first commit

---
+++ 01_Variable/src/com/kh/variableEx4.java
@@ -0,0 +1,29 @@
+//com.kh 폴더 안에 작성된 파일이라는 표기
+package com.kh;
+
+//VariableEx4 = 파일명으로 코딩을 class 내부에 작성하겠다는 표기
+public class variableEx4  {
+    //메인 메서드 : 자바 프로그램 실행용 기능
+    public static void main(String[] args) {
+
+        //상수: 한번 값을 기록하면 값을 바꿀수 없는 변수
+
+        //상수 키워드 : final 자바스크립트 const 같음
+
+        // 싱수명 작성 : 모두 대문자로 작성, 연결되는 단어 사이에는 "_"로 구분
+        // 상수 표기 자료형 변수명 = "변수값";
+
+        final String NAME = "홍길동";
+
+        final int MIN = 0;
+        final int MAX = 100;
+
+        System.out.println("최소값: "+MIN);
+        System.out.print("최대값 : " + MAX);
+
+
+
+
+
+    }
+}
